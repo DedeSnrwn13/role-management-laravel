693131eed32aa927eb3a61d0335bbbee2d03d2c1
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-01-03 09:32:01 +0700

    Membuat CRUD Article

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
                         <li class="nav-item">
                             <a href="{{ route('admin.role.index') }}" class="nav-link">Role</a>
                         </li>
+                        <li class="nav-item">
+                            <a href="{{ route('article.index') }}" class="nav-link">Article</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -70,7 +73,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                             document.getElementById('logout-form').submit();">
+                                                                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
